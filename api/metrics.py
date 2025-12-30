ec771e50678171846384db09ca74b08daf733883
Pakkiaraj T <gceraj@gmail.com>
2025-12-29 22:38:32 -0500

    1.1 initial commit

--- api/metrics.py
+++
@@ -1,9 +0,0 @@
-from prometheus_client import Counter, generate_latest
-from fastapi.responses import Response
-
-# Define metrics
-REQUEST_COUNT = Counter("api_requests_total", "Total API requests")
-
-# Metrics endpoint
-def metrics_endpoint():
-    return Response(generate_latest(), media_type="text/plain")
